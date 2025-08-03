9b4ed40b2bf0ea74741a3b5a807592c4ab859fbe
Zhenghao Liu <uth9@outlook.com>
2025-08-03 20:52:00 +0800

    User Interface Update
    更新了一点UI方面的东西

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Collections.ObjectModel;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -16,9 +18,26 @@ namespace AutoFilledIn
     /// </summary>
     public partial class MainWindow : Window
     {
+        public required List<List> StudentDataList;
+        List<string> NationList = ["汉族", "满族", "畲族"];
+        List<string> RegisYearList = ["2025"];
+        List<string> RegisMonthList = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
+
         public MainWindow()
         {
             InitializeComponent();
+
+            /// 初始化ComboBox控件数值绑定
+            NationBox.ItemsSource = NationList;
+            NationBox.SelectedIndex = 0;
+            RegisteredYearBox.ItemsSource = RegisYearList;
+            RegisteredYearBox.SelectedIndex = 0;
+            RegisteredMonthBox.ItemsSource= RegisMonthList;
+            RegisteredMonthBox.SelectedIndex = 0;
+
+            /// 初始化StudentData数值绑定
+            
+            StudentData.ItemsSource = StudentDataList;
         }
     }
 }
