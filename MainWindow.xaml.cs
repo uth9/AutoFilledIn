7f0c2be43f54558ddac7cf85c607350d36c8479d
Zhenghao Liu <uth9@outlook.com>
2025-08-04 22:47:51 +0800

    例行更改
    数据绑定问题待处理

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.Diagnostics.CodeAnalysis;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Windows;
@@ -22,7 +23,18 @@ namespace AutoFilledIn
         List<string> NationList = ["汉族", "满族", "畲族"];
         List<string> RegisYearList = ["2025"];
         List<string> RegisMonthList = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
-
+        
+        /// 初始化当前显示
+        public string CurrentName = "姓名";
+        public string CurrentDevelopedId = "id";
+        public string CurrentNation = "畲族";
+        public string CurrentIdentifyCode = "idcode";
+        public string CurrentReconfirmedId = "confirmedid";
+        public string CurrentTel = "";
+        public string CurrentAddress = "";
+        public int[] RegistedDate = {9, 23};
+        public bool CurrentVolunteerState = true;
+        
         public MainWindow()
         {
             InitializeComponent();
@@ -30,14 +42,21 @@ namespace AutoFilledIn
             /// 初始化ComboBox控件数值绑定
             NationBox.ItemsSource = NationList;
             NationBox.SelectedIndex = 0;
-            RegisteredYearBox.ItemsSource = RegisYearList;
-            RegisteredYearBox.SelectedIndex = 0;
-            RegisteredMonthBox.ItemsSource= RegisMonthList;
-            RegisteredMonthBox.SelectedIndex = 0;
+            RegistedYearBox.ItemsSource = RegisYearList;
+            RegistedYearBox.SelectedIndex = 0;
+            RegistedMonthBox.ItemsSource= RegisMonthList;
+            RegistedMonthBox.SelectedIndex = 0;
 
             /// 初始化StudentData数值绑定
-            
             StudentData.ItemsSource = StudentDataList;
+
+            /// 初始化当前显示数据绑定
+            NameBox.Text = CurrentName;
+            DevelopIdBox.Text = CurrentDevelopedId;
+            NationBox.Text = CurrentNation;
+            PersonalIdentifyCodeBox.Text = CurrentIdentifyCode;
+            ReEnterIdentifyCodeBox.Text = CurrentReconfirmedId;
+            IfRegAsVolunteerBox.IsChecked = CurrentVolunteerState;
         }
 
         private void CreateNewColumnButton_Click(object sender, RoutedEventArgs e)
--- obj/Debug/net8.0-windows/AutoFilledIn.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/AutoFilledIn.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoFilledIn")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e5b1c6e833a9b6ca742c872373d79ae9ba61894")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e670789325f47a8c1207e7952c7d8471eb416870")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoFilledIn")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoFilledIn")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
